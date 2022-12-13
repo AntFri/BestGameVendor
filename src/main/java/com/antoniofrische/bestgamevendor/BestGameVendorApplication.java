eabc74ff3c9abc533ba60e56c57ba24a9ea25ce3
AntonioFrische <antonio.frische@outlook.com>
2022-12-13 15:57:45 +0100

    init Commit

---
+++ src/main/java/com/antoniofrische/bestgamevendor/BestGameVendorApplication.java
@@ -0,0 +1,13 @@
+package com.antoniofrische.bestgamevendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestGameVendorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestGameVendorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/antoniofrische/bestgamevendor/BestGameVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniofrische.bestgamevendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestGameVendorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
