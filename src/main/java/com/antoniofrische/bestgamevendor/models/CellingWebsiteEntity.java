7baa5616d76b2d20741ddac16fd97508197264fd
AntonioFrische <antonio.frische@outlook.com>
2023-02-07 16:23:30 +0100

    add more stuff

---
+++ src/main/java/com/antoniofrische/bestgamevendor/models/CellingWebsiteEntity.java
@@ -0,0 +1,57 @@
+package com.antoniofrische.bestgamevendor.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "celling_website", schema = "gamevendor", catalog = "")
+public class CellingWebsiteEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "idcelling_Website", nullable = false)
+    private int idcellingWebsite;
+    @Basic
+    @Column(name = "nombre", nullable = true, length = 45)
+    private String nombre;
+    @Basic
+    @Column(name = "link", nullable = true, length = 45)
+    private String link;
+
+    public int getIdcellingWebsite() {
+        return idcellingWebsite;
+    }
+
+    public void setIdcellingWebsite(int idcellingWebsite) {
+        this.idcellingWebsite = idcellingWebsite;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CellingWebsiteEntity that = (CellingWebsiteEntity) o;
+        return idcellingWebsite == that.idcellingWebsite && Objects.equals(nombre, that.nombre) && Objects.equals(link, that.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idcellingWebsite, nombre, link);
+    }
+}
---
+++ src/main/java/com/antoniofrische/bestgamevendor/security/models/CustomUserDetailService.java
@@ -0,0 +1,24 @@
+package com.antoniofrische.bestgamevendor.security.models;
+
+import com.antoniofrische.bestgamevendor.models.UsuarioEntity;
+import com.antoniofrische.bestgamevendor.repositorios.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private IUserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UsuarioEntity user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+
+}
